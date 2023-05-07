0684f190808274660fb0d3eeeac168fa14d4333e
kuldeepsidhu88 <kuldeepsidhu88@gmail.com>
2023-05-07 19:40:04 +0530

    Updated Junit4 tests to Junit5

--- src/test/java/dev/hashnode/kuldeepsidhu/CalculatorTest.java
+++ src/test/java/dev/hashnode/kuldeepsidhu/CalculatorTest.java
@@ -2,57 +2,66 @@
  * Copyright 2023 License Header.
  */
 package dev.hashnode.kuldeepsidhu;
-
-import org.junit.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
     private Calculator calculator;
 
-    @BeforeClass
+    @BeforeAll
     public static void beforeClass() throws Exception {
         System.out.println("In beforeClass method");
     }
 
-    @Before
+    @BeforeEach
     public void before() throws Exception {
         System.out.println("In before method");
         calculator = new Calculator();
     }
 
-    @AfterClass
+    @AfterAll
     public static void afterClass() throws Exception {
         System.out.println("In afterClass method");
     }
 
-    @After
+    @AfterEach
     public void after() throws Exception {
         System.out.println("In after method");
     }
 
-    @Ignore
+    @Disabled
     @Test
-    public void ignoredTest() {
+    void ignoredTest() {
         // ignored test
     }
 
     @Test
-    public void testAddition() {
-        Assert.assertEquals(10, calculator.add(7, 3));
+    void testAddition() {
+        Assertions.assertEquals(10, calculator.add(7, 3));
     }
 
     @Test
-    public void testSubtraction() {
-        Assert.assertEquals(6, calculator.subtract(10, 4));
+    void testSubtraction() {
+        Assertions.assertEquals(6, calculator.subtract(10, 4));
     }
 
     @Test
-    public void testMultiply() {
-        Assert.assertEquals(56, calculator.multiply(8, 7));
+    void testMultiply() {
+        Assertions.assertEquals(56, calculator.multiply(8, 7));
     }
 
     @Test
-    public void testDivide() {
-        Assert.assertEquals(2, calculator.divide(10, 5));
+    void testDivide() {
+        Assertions.assertEquals(2, calculator.divide(10, 5));
     }
 }
